e279abd7ee41fda56657908a4b80a211801fc69d
Madhankumar Muralitharan <madhankumar.muralitharan@nutechnologyinc.com>
2022-06-22 16:19:09 +0530

    Added production build configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-var customConfig = require('./webpack.custom.js');
+const customConfig = require('./webpack.custom.js');
+
+// Detect build environment
+const env = process.env.NODE_ENV;
+const isDev = env !== 'production';
+
 const nextConfig = {
   webpack: (config) => {
     // Add your custom webpack config here
 
-    config = customConfig(config, true);
+    config = customConfig(config, isDev);
 
     return config;
   },
